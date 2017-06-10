2ac17397f109b6cc57cb899c15c539007ddc2ca5
Sam Thorogood <sam.thorogood@gmail.com>
2017-06-10 19:08:48 +1000

    support better, faster, flatter data

--- provider.js
+++ provider.js
@@ -1,54 +1,70 @@
 (function() {
-  let emojimap;
+  const api = 'https://emojityper.appspot.com';
 
-  const helper = window.fetch('https://emojityper.com/res/js/emojimap.js').then(out => out.text()).then(out => {
-    const first = out.indexOf('{');
-    if (first === -1) {
-      throw new Error('invalid emojimap');
+  function historicData() {
+    let p = window.fetch('https://emojityper.com/res/js/emojimap.js').then(out => out.text());
+    p = p.then(raw => {
+      const first = raw.indexOf('{');
+      if (first === -1) {
+        throw new Error('invalid emojimap');
+      }
+      // FIXME: never do this :O
+      eval('var emojimap = ' + raw.substr(first));
+      return emojimap;
+    })
+    p = p.then(emojimap => {
+      const out = {};
+      Object.keys(emojimap).forEach(string => {
+        const data = emojimap[string];
+        string = string.replace(/\s+/g, '').replace(/[’’]/g, '');
+        if (!(string in out)) {
+          out[string] = [];
+        }
+        out[string].push(...data.map(details => details.emoji));
+      });
+
+      return out;
+    });
+    return p;
+  }
+
+  function futureData() {
+    return window.fetch(api + '/popular').then(out => out.json());
+  }
+
+  const data = historicData();
+  let indexed = data.then(emojimap => {
+    const prefixLength = 3;   // generate prefixes up to this length
+    const maxSuggestions = 10;  // only generate this many suggestions
+    const prefixSuggest = {};
+
+    for (let k in emojimap) {
+      const prefix = k.substr(0, prefixLength);
+      for (let i = 1; i <= prefix.length; ++i) {
+        const part = prefix.substr(0, i);
+        let opts = prefixSuggest[part];
+        if (!opts) {
+          opts = prefixSuggest[part] = [];
+        }
+        if (opts.length < maxSuggestions) {
+          opts.push(k);
+        }
+      }
     }
-    // FIXME: never do this :O
-    eval('emojimap = ' + out.substr(first));
 
-    emojimap['karate'] = [
-      {
-        'emoji': '👊🏻💥',
-      },
-    ];
+    return function(typed) {
+      const rest = typed.substr(prefixLength);
+      let all = prefixSuggest[typed.substr(0, prefixLength)] || [];
 
-    return (function() {
-      const prefixLength = 3;   // generate prefixes up to this length
-      const maxSuggestions = 10;  // only generate this many suggestions
-      const prefixSuggest = {};
-      for (let k in emojimap) {
-        const prefix = k.substr(0, prefixLength);
-        for (let i = 1; i <= prefix.length; ++i) {
-          const part = prefix.substr(0, i);
-          let opts = prefixSuggest[part];
-          if (!opts) {
-            opts = prefixSuggest[part] = [];
-          }
-          if (opts.length < maxSuggestions) {
-            opts.push(k);
-          }
-        }
+      if (rest) {
+        all = all.filter(word => word.substr(prefixLength).startsWith(rest));
       }
+      all = all.map(word => {
+        return {'name': word, 'options': emojimap[word] || []};
+      });
 
-      return function(typed) {
-        const rest = typed.substr(prefixLength);
-        let all = prefixSuggest[typed.substr(0, prefixLength)] || [];
-        if (rest) {
-          all = all.filter(word => word.substr(prefixLength).startsWith(rest));
-        }
-
-        all = all.map(word => {
-          const options = (emojimap[word] || []).map(obj => obj['emoji']);
-          return {'name': word, 'options': options};
-        });
-        all = all.filter(result => result.options.length);
-
-        return all.length ? all : null;
-      }
-    }());
+      return all.length ? all : null;
+    }
   });
 
   let requestWord = null;
@@ -59,9 +75,11 @@
       return;
     }
 
-    helper.then(suggest => {
+    indexed.then(suggest => {
       const results = suggest(word);
       window.emojimanager.callback(results);
+    }).catch(err => {
+      console.warn('some error in suggest', err);
     });
   };
 
@@ -75,18 +93,10 @@
       return requestWord;
     },
     submit(name, value) {
-      // TODO: do something
-      console.debug('got', name, 'to', value);
-      return new Promise((resolve, reject) => {
-        window.setTimeout(resolve, Math.random() * 5000);
-      }).then(_ => {
-        let data = emojimap[name];
-        if (!data) {
-          emojimap[name] = data = [];
-        }
-        // TODO: doesn't update index :( - so doesn't work
-        data.push({emoji: value});
-      });
+      const data = new FormData();
+      data.set('name', name);
+      data.set('emoji', value);
+      return window.fetch(api + '/name', {method: 'POST', data});
     },
   };
 
