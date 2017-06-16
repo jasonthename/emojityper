ca4530d2ebf1145f567c396b33d96dcf9608c797
Sam Thorogood <sam.thorogood@gmail.com>
2017-06-16 23:46:04 +1000

    build steps

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+
+const less = require('gulp-less');
+const tweakdom = require('gulp-tweakdom');
+const gulp = require('gulp');
+const sourcemaps = require('gulp-sourcemaps');
+const rollup = require('gulp-better-rollup')
+const babel = require('rollup-plugin-babel')
+
+gulp.task('less', function() {
+  return gulp.src('*.less')
+      .pipe(less())
+      .pipe(gulp.dest('./dist'));
+});
+
+gulp.task('rollup', function() {
+  // TODO: bring in Promise and fetch polyfill
+  const options = {
+    plugins: [babel()],
+  };
+  gulp.src('src/bundle.js')
+      .pipe(sourcemaps.init())
+      .pipe(rollup(options, {format: 'iife'}))
+      .pipe(sourcemaps.write('.'))
+      .pipe(gulp.dest('./dist'));
+})
+
+gulp.task('html', function() {
+  const mutator = document => {
+
+  };
+  return gulp.src('*.html')
+      .pipe(tweakdom(mutator))
+      .pipe(gulp.dest('./dist'));
+})
+
+gulp.task('default', ['less', 'rollup', 'html']);
