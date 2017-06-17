aff3c4346163457ef6aa03d496bae47962b4e7f6
Sam Thorogood <sam.thorogood@gmail.com>
2017-06-17 12:16:25 +1000

    deploy script

---
+++ deploy.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -eu
+
+# check that branch switch is safe
+git checkout gh-pages
+git checkout master
+
+# build and copy
+gulp clean && gulp
+git checkout gh-pages
+cp -R dist/* .
+
+# try to checkin code, only commit if change
+git add .
+if git diff-index --quiet HEAD --; then
+  git status
+else
+  git commit -m "release $(date)"
+  git push
+fi
+
+# back to master
+git checkout master
