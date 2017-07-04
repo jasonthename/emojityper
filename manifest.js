02ef0f5f93262a48b32897cf7538585158c048f9
Sam Thorogood <sam.thorogood@gmail.com>
2017-07-04 19:14:32 +1000

    release Tue  4 Jul 2017 19:14:32 AEST

--- manifest.js
+++ manifest.js
@@ -1,54 +1,54 @@
 self.__file_manifest = [
   {
-    "url": "/bundle.min.js",
+    "url": "./bundle.min.js",
     "revision": "d5e1b2908b079ecf432dfc70d4c9400e"
   },
   {
-    "url": "/error.html",
+    "url": "./error.html",
     "revision": "c02f875801e452b88392352e67ea1942"
   },
   {
-    "url": "/index.html",
+    "url": "./index.html",
     "revision": "f6c08fc56cb393f8a994d5eb0e9800d3"
   },
   {
-    "url": "/manifest.json",
+    "url": "./manifest.json",
     "revision": "5d901073fd2cf611e2e2593945dce6e4"
   },
   {
-    "url": "/opensearch.xml",
+    "url": "./opensearch.xml",
     "revision": "37f41b8cc7e1383874350f6f3bbe1014"
   },
   {
-    "url": "/res/crying.png",
+    "url": "./res/crying.png",
     "revision": "cfb9138bebe7aca2609f1ddc22f89079"
   },
   {
-    "url": "/res/icon-128.png",
+    "url": "./res/icon-128.png",
     "revision": "4ede97c725914d6b563ecbebc79ab65f"
   },
   {
-    "url": "/res/icon-192.png",
+    "url": "./res/icon-192.png",
     "revision": "78015ecb19c885bbda9739f3bdd23a19"
   },
   {
-    "url": "/res/icon-256.png",
+    "url": "./res/icon-256.png",
     "revision": "86f6d723d862012ab85535108000ef25"
   },
   {
-    "url": "/res/icon-512.png",
+    "url": "./res/icon-512.png",
     "revision": "0b23c85f71f7a57e28cc9e897f9ffbe0"
   },
   {
-    "url": "/styles.css",
+    "url": "./styles.css",
     "revision": "b609a4222405e847d13de8467beea72f"
   },
   {
-    "url": "/support.min.js",
+    "url": "./support.min.js",
     "revision": "ffca18d3a34864b7f1aeb6195e1e844a"
   },
   {
-    "url": "/sw.js",
-    "revision": "7b4440135ea7a07ef446d1aef853b03d"
+    "url": "./sw.js",
+    "revision": "dc3be6617981a8fb8b85bf00307b9a92"
   }
 ];
