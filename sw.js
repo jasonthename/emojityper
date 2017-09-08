63880bc750c42cd7b3d03cd2fc0224bed6836613
Sam Thorogood <sam.thorogood@gmail.com>
2017-09-09 08:36:18 +1000

    release Sat  9 Sep 2017 08:36:18 AEST

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 /**
- * Generated on Mon Sep 04 2017 11:38:33 GMT+1000
+ * Generated on Sat Sep 09 2017 08:36:15 GMT+1000
  */
 
-importScripts('https://unpkg.com/workbox-sw@1.0.1');
+importScripts('https://unpkg.com/workbox-sw@2.0.1/build/importScripts/workbox-sw.prod.v2.0.1.js');
 importScripts('./manifest.js');
 
 const workboxSW = new self.WorkboxSW();
@@ -16,7 +16,7 @@ const realURLs = [
 function escape(s) {
   return s.replace(/[-\/\\^$*+?.()|[\]{}]/g, '\\$&');
 }
-realURLs.forEach(prefix => {
+realURLs.forEach((prefix) => {
   // TODO: This only caches the request on the 2nd load. However it's probably coming out of cache
   // for a while anyway because the resources are served with stupid high expiries.
   const r = new RegExp('^' + escape(prefix) + '.*');
