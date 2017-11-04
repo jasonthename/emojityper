9bfb218b655fb40fa66d1d2f6c2dcd7f3d7089f5
Sam Thorogood <sam.thorogood@gmail.com>
2017-11-04 19:31:19 +1100

    simplify resize since we have a .toggle polyfill

--- src/page.js
+++ src/page.js
@@ -1,20 +1,16 @@
 
-const value = ev => {
+const value = (ev) => {
   const text = ev.detail.trim();
-  // we don't use .toggle, as it has an IE11 bug
-  if (text) {
-    document.body.classList.add('has-value');
-  } else {
-    document.body.classList.remove('has-value');
-  }
+  document.body.classList.toggle('has-value', Boolean(text));
 };
 typer.addEventListener('value', value);
 value({detail: typer.value});
 
 // set minHeight to actual viewport height, but allow for keyboard etc
-const resize = ev => {
+const resize = (ev) => {
   const height = window.innerHeight;
   document.body.style.minHeight = `${height}px`;
 };
 window.addEventListener('resize', resize);
+window.addEventListener('load', resize);
 resize();
