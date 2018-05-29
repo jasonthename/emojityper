ed7e6a98cd9afd5aca10c93ec616226980c92eac
Sam Thorogood <sam.thorogood@gmail.com>
2018-05-29 12:17:13 +1000

    gulp simplify/inline css

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,7 @@ const less = require('gulp-less');
 const sourcemaps = require('gulp-sourcemaps');
 const tweakdom = require('gulp-tweakdom');
 const path = require('path');
+const fs = require('fs');
 const babel = require('rollup-plugin-babel')
 const commonJS = require('rollup-plugin-commonjs');
 const uglify = require('rollup-plugin-uglify');
@@ -42,7 +43,7 @@ gulp.task('rollup-nomodule', function() {
       babel(),
       uglify({output: {ascii_only: true}}),
     ],
-    cache: false,  // cache clobbers rollup
+    cache: false,  // cache clobbers other rollup tasks
   };
   return gulp.src(['src/support/*.js', 'src/bundle.js'])
     .pipe(sourcemaps.init())
@@ -53,13 +54,12 @@ gulp.task('rollup-nomodule', function() {
 })
 
 gulp.task('rollup', function() {
-  // nb. this task doesn't really depend on rollup-nomodule, but they can't be run in parallel
   const options = {
     // nb. ascii_only so escaped emoji are left alone
     plugins: [
       uglify({output: {ascii_only: true, ecma: 6}, mangle: {safari10: true}}, uglifyES.minify),
     ],
-    cache: false,  // cache clobbers rollup-nomodule
+    cache: false,  // cache clobbers other rollup tasks
   };
   return gulp.src('src/bundle.js')
     .pipe(sourcemaps.init())
@@ -69,15 +69,14 @@ gulp.task('rollup', function() {
     .pipe(gulp.dest('./dist'));
 });
 
-gulp.task('html', function() {
+gulp.task('html', ['css'], function() {
   const mutator = (document, file) => {
-    if (path.basename(file) !== 'index.html') { return; }
-
     // replace lessCSS with actual styles
+    // FIXME(samthor): We generate the dist file, but just inline it anyway.
     document.getElementById('less').remove();
-    const link = Object.assign(document.createElement('link'),
-        {href: `styles-${nonce}.css`, rel: 'stylesheet'});
-    document.head.appendChild(link);
+    const raw = fs.readFileSync(`./dist/styles-${nonce}.css`);
+    const style = Object.assign(document.createElement('style'), {textContent: raw});
+    document.head.appendChild(style);
 
     // fix paths for module/nomodule code
     document.head.querySelector('script[nomodule]').src = `support-${nonce}.min.js`;
@@ -86,7 +85,7 @@ gulp.task('html', function() {
     // append buildAt
     document.head.appendChild(document.createComment(`Generated on: ${builtAt}`));
   };
-  return gulp.src('*.html')
+  return gulp.src('index.html')
     .pipe(tweakdom(mutator))
     .pipe(gulp.dest('./dist'));
 });
@@ -96,6 +95,7 @@ gulp.task('static', function() {
     'CNAME',
     'manifest.json',
     'opensearch.xml',
+    'error.html',
     'google*.html',
     'res/*',
   ];
@@ -113,12 +113,16 @@ gulp.task('clean', function() {
 gulp.task('preparedist', ['clean', 'css', 'js', 'html', 'static']);
 
 gulp.task('sw', ['preparedist'], async function() {
+  const globPatterns = [
+    '**/*.{js,json,png}',  // nb. doesn't include css, which is inlined
+    'index.html',          // don't include error/google html
+  ];
   const {count, size} = await workbox.injectManifest({
     swSrc: 'sw.js',
     swDest: './dist/sw.js',
     globDirectory: './dist',
     globIgnores: ['sw.js'],  // don't include SW
-    globPatterns: ['**/*.{png,html,js,json,css}'],
+    globPatterns,
     modifyUrlPrefix: {'/': './'},  // treat files as relative to SW
   });
 });
