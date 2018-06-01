71031b3d22086d85a16b415dae5023a9365a3783
Sam Thorogood <sam.thorogood@gmail.com>
2018-06-02 09:45:58 +1000

    check controller on initial load

--- src/sw.js
+++ src/sw.js
@@ -4,11 +4,14 @@ if (navigator.serviceWorker) {
     console.warn('failed to register SW', err);
   });
 
+  const hadInitialController = Boolean(navigator.serviceWorker.controller);
   navigator.serviceWorker.addEventListener('controllerchange', () => {
-    if (navigator.serviceWorker.controller) {
-      // only reload if we already had a SW
+    if (hadInitialController) {
+      // only reload if we had a SW on load
       console.debug('got SW controllerchange, reload');
       window.location.reload();
+    } else {
+      console.info('NOT reloading')
     }
   });
 }
