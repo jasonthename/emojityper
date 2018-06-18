dd8343d2813a74f574265256544c425297292627
Sam Thorogood <sam.thorogood@gmail.com>
2018-06-18 18:12:50 +1000

    global event listener

--- src/bundle.js
+++ src/bundle.js
@@ -8,3 +8,11 @@ import './page.js';
 import './adverts.js';
 import './sw.js';
 import './offline.js';
+
+// global error handler for logs
+window.onerror = (msg, file, line, col, error) => {
+  console.info('got err', String(msg));
+  try {
+    ga('send', 'event', 'error', `${file},${line}:${col}`, String(msg), {nonInteraction: true});
+  } catch (e) {}
+};
